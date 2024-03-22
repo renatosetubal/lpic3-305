76c2a2b8b9a5781e9073cd8e5878e0e76821bf2e
Renato Miranda Setúbal <renato.setubal@gmail.com>
2024-03-22 16:23:03 -0300

    Reorganização de código

--- Debian12/scripts/db12xen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get update -y
-mkfs.xfs /dev/sdb
-mkdir /mnt/sdb -p
-mount /dev/sdb /mnt/sdb
-echo "/dev/sdb /mnt/sdb xfs defaults 0 0" >> /etc/fstab
-systemctl daemon-reload
-mount -a
