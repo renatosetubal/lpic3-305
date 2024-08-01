6dc2ae06a8b93b0ca1d0c29340227411a32e3fad
Renato Miranda Setúbal <remiset@gmail.com>
2024-08-01 14:06:32 -0300

    Modificacao de machines

--- Arquivos/debxen/installxen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install -y xen-system-amd64 xen-tools
-echo 'GRUB_CMDLINE_XEN_DEFAULT="dom0_max_vcpus=1 dom0_mem=1014M,max:1024M"' >> /etc/default/grub
-update-grub
-reboot
